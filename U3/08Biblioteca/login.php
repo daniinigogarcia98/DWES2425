6af962b900a8d58bd2767a78e905a389aa0f5966
Daniel Iñigo Garcia <daniinigogarcia98@gmail.com>
2024-10-16 12:44:39 +0200

    login biblioteca

---
+++ U3/08Biblioteca/login.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+  </head>
+  <body>
+  <div class="container">
+
+<p class="display-2">Biblioteca - Login</p>
+<form width="50%">
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Usuario</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">Nunca compartiremos su usuario con nadie más.</div>
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+        <div id="emailHelp" class="form-text">Nunca compartiremos su contraseña con nadie más.</div>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Entrar</button>
+</form>
+</div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.6.0.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>

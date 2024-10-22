68a73be30486da7c06f44e4bc4febdd7893cfc0a
Daniel Iñigo Garcia <daniinigogarcia98@gmail.com>
2024-10-22 08:54:13 +0200

    Biblioteca con boostrap

--- U3/08Biblioteca/login.php
+++ U3/08Biblioteca/login.php
@@ -1,64 +1,60 @@
 <?php
 require_once 'Modelo.php';
 session_start();
-if(isset($_SESSION['usuario'])){
-    //Redirigimos si ya estamos logueados
+if (isset($_SESSION['usuario'])) {
+    // Redirigimos si ya estamos logueados
     header('location:prestamos.php');
+    exit();
 }
-if(isset($_POST['entrar'])){
+if (isset($_POST['entrar'])) {
     $bd = new Modelo();
-    if($bd->getConexion()==null){
+    if ($bd->getConexion() == null) {
         $error = 'Error, no se puede conectar con la BD';
-    }
-    else{
-        //Comprobar usuario y ps y si los datos son correctos
-        //Guardamos el usuario en una sesión y redirigimos
-        //a la página préstamos.php
-        $us = $bd->loguear($_POST['usuario'],$_POST['ps']);
-        if($us!=null){
-            //Almacenamos en la sesión
+    } else {
+        // Comprobar usuario y ps
+        $us = $bd->loguear($_POST['usuario'], $_POST['ps']);
+        if ($us != null) {
+            // Almacenamos en la sesión
             $_SESSION['usuario'] = $us;
-            //Redirigimos
+            // Redirigimos
             header('location:prestamos.php');
-        }
-        else{
-            $error='Error, datos incorrectos';
+            exit();
+        } else {
+            $error = 'Error, datos incorrectos';
         }
     }
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Biblioteca</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <title>Biblioteca - Login</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery-3.6.0.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </head>
 
 <body>
-    <div class="container">
-
-    <p class="display-2">Biblioteca - Login</p>
-        <form action="" method="post">
+    <div class="container mt-5">
+        <h1 class="text-center mb-4">Biblioteca - Login</h1>
+        <form action="" method="post" class="card p-4 shadow">
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Usuario</label>
-                <input type="text" name="usuario" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                <label for="usuario" class="form-label">Usuario</label>
+                <input type="text" name="usuario" class="form-control" id="usuario" required />
             </div>
             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" name="ps" class="form-control" id="exampleInputPassword1">
+                <label for="password" class="form-label">Contraseña</label>
+                <input type="password" name="ps" class="form-control" id="password" required />
             </div>
-        
-            <button type="submit" class="btn btn-primary" name="entrar">Entrar</button>
+            <button type="submit" class="btn btn-primary w-100" name="entrar">Entrar</button>
         </form>
-        <?php
-        if(isset($error)){
-            echo '<div class="text-danger">'.$error.'</div>';
-        }
-        ?>
+        <?php if (isset($error)): ?>
+            <div class="text-danger text-center mt-3"><?php echo $error; ?></div>
+        <?php endif; ?>
     </div>
 </body>
 

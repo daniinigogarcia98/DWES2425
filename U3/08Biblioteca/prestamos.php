fff05195ae5b1f2c259889c7580863d2af6231b2
Daniel Iñigo Garcia <daniinigogarcia98@gmail.com>
2024-10-27 18:18:04 +0100

    sql actualizado y biblioteca actualizada

--- U3/08Biblioteca/prestamos.php
+++ U3/08Biblioteca/prestamos.php
@@ -8,8 +8,25 @@ require_once 'controlador.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biblioteca</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link href="css/bootstrap.css" rel="stylesheet"/>
+    <link href="css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="css/bootstrap.rtl.css" rel="stylesheet"/>
+    <link href="css/bootstrap.rtl.min.css" rel="stylesheet"/>
+    <link href="css/bootstrap-grid.css" rel="stylesheet"/>
+    <link href="css/bootstrap-grid.min.css" rel="stylesheet"/>
+    <link href="css/bootstrap-grid.rtl.css" rel="stylesheet"/>
+    <link href="css/bootstrap-grid.rtl.min.css" rel="stylesheet"/>
+    <link href="css/bootstrap-reboot.css" rel="stylesheet"/>
+    <link href="css/bootstrap-reboot.rtl.css" rel="stylesheet"/>
+    <link href="css/bootstrap-reboot.css" rel="stylesheet"/>
+    <link href="css/bootstrap-reboot.min.css" rel="stylesheet"/>
+    <link href="css/bootstrap-utilities.css" rel="stylesheet"/>
+    <link href="css/bootstrap-utilities.rtl.css" rel="stylesheet"/>
+    <link href="css/bootstrap-utilities.css" rel="stylesheet"/>
+    <link href="css/bootstrap-utilities.min.css" rel="stylesheet"/>
+    <link href="css/bootstrap-utilities.min.css" rel="stylesheet"/>
+    <link href="css/bootstrap-utilities.rtl.min.css" rel="stylesheet"/>
+    <script src="js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>

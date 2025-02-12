bf3cd2d68e106d77af757da5a0c40ed0cd301661
Daniel Iñigo Garcia <daniinigogarcia98@gmail.com>
2025-02-12 11:15:40 +0100

    agregar validaciones y estructura inicial para el registro de usuarios

--- U6/ofertas/controllers/usuarioC.js
+++ U6/ofertas/controllers/usuarioC.js
@@ -1,8 +1,20 @@
+//Importar el modelo de usuario
+const Usuario = require('../models/usuario');
+//Importar bcyrpt
+const cifrar = require('bcrypt');
 function login(req, res) {
     res.status(200).send('Página de login');
 }
 
 function registro(req, res) {
+     //Recuperar los datos de la solicitud (req)
+     const {nombre, email, password,perfil} = req.body;
+
+     //Validar si vienen todos los datos para el registro
+     if(!nombre || !email || !password || !perfil){
+         throw{textoError: 'Faltan datos para registrar al usuario'};
+     }
+     //Comprobar que no hay otro usuario con ese emasil
     res.status(200).send('Página de registro');
 }
 

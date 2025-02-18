f825ebc7a217261ef2ef289b737a2383250f5690
Daniel Iñigo Garcia <daniinigogarcia98@gmail.com>
2025-02-18 22:38:34 +0100

    repaso examen t4

---
+++ U4/conciertos.sql
@@ -0,0 +1,6 @@
+-- ejecutar php artisan migrate en el proyecto conciertos y luego el use y por ultimo el insert
+use concietos;
+INSERT INTO `conciertos` VALUES 
+(9,'2025-01-24 23:00:00','2025-01-24 23:00:00','Canta Juegos','2025-01-25',200,10),
+(10,'2025-01-24 23:00:00','2025-01-24 23:00:00','Mucho más que tres - Abigail','2025-02-02',170,21),
+(11,'2025-01-24 23:00:00','2025-01-24 23:00:00','DJ Noon','2025-03-03',100,12);
